278c209821679ec8f9d42d25b662c3ec5fd33d93
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-02-25 08:45:09 +0000

    feat: creazione txt e aggiunta istruzione

---
+++ Exceptions/ReadFiles.java
@@ -0,0 +1,24 @@
+public package Exceptions;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+public class ReadFiles {
+    public static void main(String[] args){
+        try {
+            File file = new File("carmelo.txt");
+            Scanner scanner = new Scanner(file);
+            int n = 0;
+            while (scanner.hasNextLine()) {
+                n++;
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+            System.out.println("Il file contiene " + n + "righe");
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato");
+        } catch (NoSuchElementException e){
+            System.out.println("Nel file non sono presenti elementi");
+        }
+    } 
+}
