bf87a3bc22beaff2df9f1486b2c535b457ceab31
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-02-25 08:57:35 +0000

    feat: aggiornamento della classe ReadFiles per sommare i valori letti e rimozione del file TriangoloIsoscele

--- Exceptions/ReadFiles.java
+++ Exceptions/ReadFiles.java
@@ -4,18 +4,24 @@ import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 public class ReadFiles {
+    public final static int MAX_LINES = 100;
     public static void main(String[] args){
+        int[] buffer = new int[MAX_LINES];
+        int sum = 0;
         try {
             File file = new File("carmelo.txt");
             Scanner scanner = new Scanner(file);
             int n = 0;
             while (scanner.hasNextLine()) {
-                n++;
                 String row = scanner.nextLine();
+                buffer[n] = Integer.parseInt(row);
+                n++;
+                sum += buffer[n];
                 System.out.println(row);
             }
             scanner.close();
-            System.out.println("Il file contiene " + n + "righe");
+            System.out.println("Il file contiene " + n + " righe");
+            System.out.println("La somma è " + sum );
         } catch (FileNotFoundException e) {
             System.out.println("File non trovato");
         } catch (NoSuchElementException e){
