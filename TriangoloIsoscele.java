edf0177423566032bc1e10403d75b2918e5f884c
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-02-25 08:34:46 +0000

    v c

---
+++ TriangoloIsoscele.java
@@ -0,0 +1,11 @@
+public class TriangoloIsoscele{
+    public static float tBase;
+    public static float tBase;
+
+    public static void main(String[] args){
+        
+    }
+    public static float perimetro(){
+
+    }
+}
