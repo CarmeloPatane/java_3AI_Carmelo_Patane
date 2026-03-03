928d20b0655cf76ea4db3b5355839bf26eff41b4
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-03-03 10:48:16 +0000

    feat: implementazione metodo riempiMatriceConCaratteri

--- Codes/Matrice.java
+++ Codes/Matrice.java
@@ -7,10 +7,13 @@ public class Matrice{
     }
     //metodi
     //metodo per riempire mat4rice con caratteri casuali
-    public char[] riempiMatriceConCaratteri(char[] matrix){
+    public char[][] riempiMatriceConCaratteri(char[][] matrix){
         //Per ogni elemento della matrice bisogna inserire un carattere casuale
         for(int i = 0; i < matrix.length; i++){
-            
+            for(int j = 0; j < matrix.length; j++){
+                //Dopo si esegue il casting
+                matrix[i][j] = (char) random.nextInt(65, 123);
+            }
         }
         return matrix;
     }
