a1b667480a7d33d8a5997a1d1aef8269dc47b1dc
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-03-03 17:56:54 +0000

    feat: completamento tusse 1

--- Codes/Toussain_1.java
+++ Codes/Toussain_1.java
@@ -6,44 +6,48 @@ import java.util.Random;
     stampare array in ordine e il risultato
  */
 public class Toussain_1{
-        public static void main (String[]args){
-            //creazione dei due array
-            Scanner scanner = new Scanner (System.in);
-            Random random = new Random();
-            int n;
-        // riempimento dei vettori
-            System.out.println("inserisci la dimensione del primo vettore");
-            n = scanner.nextInt();
-            int [] vettore = new int [n];
-            for(int i =0; i<vettore.length;i++){
-                vettore[i] = random.nextInt(10)+1;
-            }
-            int n1;
-            System.out.println("inserisci la dimensione del secondo vettore");
-            n1= scanner.nextInt();
-            int[] vettore1 = new int [n1];
-            for(int i=0; i<vettore1.length;i++){
-                vettore1[i] = random.nextInt(10)+1;
-            }
-        // somma dei due vettori
-        int sum = 0;
-        for(int i = 0; i<vettore.length && i<vettore1.length; i++){
-            sum += vettore[i] + vettore1[i];
+    public static void main(String[] args) {
+         int [] vettore = riempiVettore();
+         stampaVettore(vettore,vettore);
+         int sum = sommaVettori(vettore, vettore);
+        System.out.println("la somma dei vettori è " +sum);
+        System.out.println();
+     }
+    
+    public static int[] riempiVettore(){
+        Scanner scanner = new Scanner (System.in);
+        Random random = new Random();
+        int n;
+        System.out.println("inserisci la dimensione dei vettori");
+        n = scanner.nextInt();
+        int[] vettore = new int [n];
+        for(int i=0; i<vettore.length;i++){
+            vettore[i] = random.nextInt(10)+1;
         }
-        
-        //stampa del primo vettore
+        scanner.close();
+        return vettore;
+    }
+    public static int sommaVettori(int[]v1,int[]v2){
+        int somma = 0;
+        for(int i = 0; i<v1.length;i++){
+            somma += v1[i] +v2[i];
+        }
+        return somma;
+    }
+    public static void stampaVettore(int[]array, int [] array2){
+        System.out.print("vettore 1: [" );
+        for(int i =0; i<array.length; i++){
+            System.out.print(array[i]+ ",");
+        }
+        System.out.println("]");
+
+        System.out.print("vettore 2:" );
         System.out.print("[");
-        for(int i =0; i<vettore.length;i++){
-            System.out.print(vettore[i]+ ",");
+        for(int i =0; i<array2.length; i++){
+            System.out.print(array2[i]+ ",");
         }
-        System.out.print("]");
-        //stampa del secondo vettore
-        System.out.print("[");
-        for(int i=0; i<vettore1.length;i++){
-            System.out.print(vettore1[i]+ ",");  
-        }
-        System.out.print("]");
-        //stampa della somma
-        System.out.println("la somma dei due vettori è " + sum);
+        System.out.println("]");
     }
 }
+            
+      
