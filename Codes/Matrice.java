0f7ffaef0b0b102b34f23d167e0137dadb9aa81f
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-03-03 10:35:59 +0000

    feat: aggiunta classe Matrice con metodo per riempire la matrice con caratteri casuali

---
+++ Codes/Matrice.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Matrice{
+    public static Random random = new Random();
+    public static void main(String[] args){
+
+    }
+    //metodi
+    //metodo per riempire mat4rice con caratteri casuali
+    public char[] riempiMatriceConCaratteri(char[] matrix){
+        //Per ogni elemento della matrice bisogna inserire un carattere casuale
+        for(int i = 0; i < matrix.length; i++){
+            
+        }
+        return matrix;
+    }
+}
